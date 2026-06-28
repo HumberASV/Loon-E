50e45dfdfc416122e9b63a955512ccdf3fc11756
Carson Fujita <50027005+TheFujirose@users.noreply.github.com>
2026-06-28 16:41:05 -0400

    Feat: auto adb commands and keyboard interrupt

--- src/loone/loone/phone.py
+++ src/loone/loone/phone.py
@@ -3,15 +3,28 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import numpy as np
 import socket
+import subprocess
 import threading
 
 class Phone(Node):
+
+    # Class Constants
+    HOST = "127.0.0.1" # Local Host
+    PORT = 5000 # Port: used for rerouting phone to computer
+
     def __init__(self):
+        """
+        Initialize the Phone node.
+        Node uses Android Debug Bridge (ADB) to get the phone's GPS coordinates, speed, and heading.
+        """
         self.heading = np.nan
         self.speed = np.nan
         self.latitude = np.nan
         self.longitude = np.nan
-        
+
+        self.get_adb_devices()
+        self.route_adb()
+
         super().__init__('Phone_Pub')
         self.publisher_ = self.create_publisher(Float32MultiArray, 'phone', 10)
         self.receiver = threading.Thread(target = self.get_odometry, daemon = True)
@@ -24,12 +37,11 @@ class Phone(Node):
         #self.get_logger().info(f"Phone: {msg.data}")
     
     def get_odometry(self):
-        HOST = "127.0.0.1"
-        PORT = 5000
+
 
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((HOST, PORT))
+        server.bind((self.HOST, self.PORT))
         server.listen(1)
 
         while rclpy.ok():
@@ -66,13 +78,36 @@ class Phone(Node):
                                 continue
                             
                             self.publish()
+    
+    def get_adb_devices(self):
+        # Execute native 'adb devices' in terminal
+        output = subprocess.check_output(["adb","devices"]).decode("utf-8")
+
+        # Parse lines and omit header
+        lines = output.strip().split("\n")[1:]
+
+        # Isolate the serial identifiers
+        devices = [line.split()[0] for line in lines if line.strip()]
+        return devices
+
+    def route_adb(self):
+        # clean slate
+        subprocess.run(["adb","reverse","--remove-all"])
+
+        # Route the localhost to computer local host
+        subprocess.run(["adb", "reverse", f"tcp:{self.PORT}", f"tcp:{self.PORT}"])
+        self.get_logger().info(f"Routed phone localhost:{self.PORT} to computer localhost:{self.PORT}")
 
 def main(args = None):
     rclpy.init(args = args)
     phone = Phone()
-    rclpy.spin(phone)
-    phone.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(phone)
+    except KeyboardInterrupt:
+        phone.get_logger().info("Phone node interrupted by user.")
+    finally:
+        phone.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
